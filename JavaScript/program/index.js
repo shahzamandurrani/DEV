ed7bed37797af0f292e7188635544ccbb9b2e591
Shahzaman Durrani <shahzamandurrani01@gmail.com>
2026-06-27 09:48:19 +0500

    created calculator

--- JavaScript/program/index.js
+++
@@ -1,9 +0,0 @@
-function welcome() {
-    let name = "Shahzaman"
-    greetings = "Salam, "
-    myGreeting = document.getElementById("welcome-el")
-    myGreeting.innerText = greetings+name
-    myGreeting.innerText += "😊"
-}
-
-welcome()
