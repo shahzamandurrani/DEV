516d67014322373caeaaafd2a2d35f77f5dce9e7
Shahzaman Durrani <shahzamandurrani01@gmail.com>
2026-07-11 13:11:32 +0500

    deployed extension

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,7 +1,25 @@
 let myLeads = []
 const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
+const ulEl = document.querySelector("#ul-el")
+
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
 })
+
+function renderLeads() {
+    let listItems = ""
+    for(let i = 0; i < myLeads.length; i++){
+        listItems += `
+        <li>
+            <a target="_blank" href='https://${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
